8c092dd598f805179d273b393c1f90682c1a1099
unknown <covarrubias.seb@gmail.com>
2023-05-22 16:00:07 -0700

    Created user location form input (from end).

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [location, setLocation] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(location);
+    // Here you will call the function to send a request to the server
+  };
+
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <h1>Weather App</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter your location"
+          value={location}
+          onChange={e => setLocation(e.target.value)}
+        />
+        <button type="submit">Get Weather</button>
+      </form>
     </div>
   );
 }
