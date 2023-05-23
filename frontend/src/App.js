0eb32fe08fd5343a7d5e3cabc03742e53c3eca52
unknown <covarrubias.seb@gmail.com>
2023-05-22 17:49:14 -0700

    Created user location input form using Google Maps API for form data validation on front end.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 function App() {
   const [location, setLocation] = useState('');
+  const [hasSelected, setHasSelected] = useState(false);
+  const autocompleteInputRef = useRef(null);
+
+  useEffect(() => {
+    const autocomplete = new window.google.maps.places.Autocomplete(autocompleteInputRef.current);
+    
+    autocomplete.addListener("place_changed", () => {
+      const place = autocomplete.getPlace();
+      if (!place.place_id) {
+        alert("Please select a location from the dropdown.");
+        return;
+      }
+      setLocation(place.formatted_address);
+      setHasSelected(true);
+    });
+  }, []);
+
+  const handleChange = (event) => {
+    setLocation(event.target.value);
+    setHasSelected(false);
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    if (!hasSelected) {
+      alert("Please select a valid location before submitting.");
+      return;
+    }
     console.log(location);
-    // Here you will call the function to send a request to the server
+    // Later here you will call the function to send a request to the server
   };
 
   return (
@@ -14,10 +39,11 @@ function App() {
       <h1>Weather App</h1>
       <form onSubmit={handleSubmit}>
         <input
+          ref={autocompleteInputRef}
           type="text"
           placeholder="Enter your location"
           value={location}
-          onChange={e => setLocation(e.target.value)}
+          onChange={handleChange}
         />
         <button type="submit">Get Weather</button>
       </form>
